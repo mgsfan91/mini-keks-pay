7eca8ece24cf04b8b4dec740f9b446ce401c6fe1
mgsfan91 <ivan.rubil@yahoo.com>
2019-10-13 22:26:07 +0200

    init

---
+++ src/main/java/com/example/minikekspay/MiniKeksPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.minikekspay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniKeksPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniKeksPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minikekspay/MiniKeksPayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.minikekspay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiniKeksPayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
