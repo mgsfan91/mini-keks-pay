9b82b8ced457b4a9498cbe3d98216a22b0921318
mgsfan91 <ivan.rubil@yahoo.com>
2019-10-15 01:33:17 +0200

    add more logging - around controller & in missing client exception

--- src/main/java/com/example/minikekspay/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/minikekspay/exception/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.example.minikekspay.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<String> handleClientNotFoundException(ClientNotFoundException exc) {
+        log.error("ClientNotFoundException " + exc.getMessage());
         return new ResponseEntity<>(exc.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
